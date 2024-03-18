7f2d3fd353cdfc6cdd00b9388503ed3a957a2dbf
Marcelo Nascimento <marcelohenrique7x@gmail.com>
2024-03-18 10:55:48 -0300

    align and size

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,19 +6,19 @@ import SocialButtons from "./SocialButtons";
 
 const Banner: React.FC<{}> = () => {
   return (
-    <div className="h-screen flex " >
-      <div className="flex flex-row items-center justify-center  px-20 z-[20]">
+    <div className="h-screen w-screen flex flex-col items-center justify-center" >
+      <div className="flex flex-row items-center justify-center z-[20]">
         <div className="flex flex-col  justify-center  text-center">
-          <div className="flex flex-col gap-6 mt-6 cursor-pointer tracking-tighter text-5xl font-semibold text-white/50 max-w-[800px] w-auto h-auto pr-56">
+          <div className="flex flex-col gap-6 mt-6 cursor-pointer tracking-tighter text-5xl font-semibold text-white/50 c sm:pr-0 md:pr-56">
             Fullstack
           </div>
-          <div className="flex flex-col gap-6  cursor-pointer tracking-tighter text-6xl font-semibold text-white/75 max-w-[800px] w-auto h-auto pr-28">
+          <div className="flex flex-col gap-6  cursor-pointer tracking-tighter text-6xl font-semibold text-white/75 c sm:pr-0 md:pr-28">
             Developer
           </div>
-          <div className="flex flex-col gap-6 cursor-pointer tracking-tighter text-8xl font-semibold text-white max-w-[800px] w-auto h-auto">
+          <div className="flex flex-col gap-6 cursor-pointer tracking-tighter text-8xl font-semibold text-white c">
             Marcelo
           </div>
-          <p className="text-md text-center text-gray-200 mt-10 my-5 max-w-[800px] h-[120px] animate-typing overflow-hidden">
+          <p className="text-lg text-center text-gray-200 mt-10 my-5 sm:h-[180px] md:h-[120px] px-3 animate-typing overflow-hidden">
             <Typewriter
               options={{
                 delay: 30,
