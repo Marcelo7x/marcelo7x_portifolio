baaedb6a91edd2fd969f97686783b718c34ebd6b
Marcelo Nascimento <marcelohenrique7x@gmail.com>
2024-03-19 21:23:21 -0300

    collor and zoom

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,62 +10,37 @@ const Navbar: React.FC<{}> = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const scrollToElement = (id: string) => {
+    toggleMenu();
+    const element = document.getElementById(id);
+    if (element) {
+      window.scroll({
+        top: element.offsetTop - 40,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div className="w-full h-[${!menuOpen? 100: 140}px] bg-['#111'] fixed backdrop-blur-sm z-50 px-8 py-2">
+    <div className={`w-full h-${menuOpen ? 140 : 100}px bg-['#111'] fixed backdrop-blur-sm z-50 px-8 py-2`}>
       <div className="w-full h-full flex flex-row items-center justify-between m-auto">
         <a
-          className="h-auto w-auto flex flex-row items-center text-xl text-white font-bold cursor-pointer"
-          onClick={() => {
-            const element = document.getElementById("banner");
-            element?.scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-          }}
+          className="h-auto w-auto flex flex-row items-center text-xl font-bold cursor-pointer hover:scale-110 transition-transform text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-orange-400"
+          onClick={() => scrollToElement("banner")}
         >
           Marcelo7x
         </a>
 
         <div className="flex flex-row gap-5 items-center">
           <div className="hidden sm:flex gap-5 text-xl font-medium">
-            {/* Links de navegação */}
-            <a
-              className="text-white cursor-pointer"
-              onClick={() => {
-                const element = document.getElementById("about");
-                if (!element) return;
-                window.scroll({
-                  top: element.offsetTop - 40,
-                  behavior: "smooth",
-                });
-              }}
-            >
+
+            <a className="text-white cursor-pointer hover:scale-110 transition-transform" onClick={() => scrollToElement("about")}>
               Sobre
             </a>
-            <a
-              className="text-white cursor-pointer"
-              onClick={() => {
-                const element = document.getElementById("experience");
-                if (!element) return;
-                window.scroll({
-                  top: element.offsetTop - 40,
-                  behavior: "smooth",
-                });
-              }}
-            >
+            <a className="text-white cursor-pointer hover:scale-110 transition-transform" onClick={() => scrollToElement("experience")}>
               Experiência
             </a>
-            <a
-              className="text-white cursor-pointer"
-              onClick={() => {
-                const element = document.getElementById("projects");
-                if (!element) return;
-                window.scroll({
-                  top: element.offsetTop - 40,
-                  behavior: "smooth",
-                });
-              }}
-            >
+            <a className="text-white cursor-pointer hover:scale-110 transition-transform" onClick={() => scrollToElement("projects")}>
               Projetos
             </a>
           </div>
@@ -81,46 +56,13 @@ const Navbar: React.FC<{}> = () => {
       {menuOpen && (
         <div className="sm:hidden">
           <div className="flex flex-col items-center gap-2">
-            <a
-              className="text-white text-xl font-medium cursor-pointer"
-              onClick={() => {
-                toggleMenu();
-                const element = document.getElementById("about");
-                if (!element) return;
-                window.scroll({
-                  top: element.offsetTop - 40,
-                  behavior: "smooth",
-                });
-              }}
-            >
+            <a className="text-white text-xl font-medium cursor-pointer" onClick={() => scrollToElement("about")}>
               Sobre
             </a>
-            <a
-              className="text-white text-xl font-medium cursor-pointer"
-              onClick={() => {
-                toggleMenu();
-                const element = document.getElementById("experience");
-                if (!element) return;
-                window.scroll({
-                  top: element.offsetTop - 40,
-                  behavior: "smooth",
-                });
-              }}
-            >
+            <a className="text-white text-xl font-medium cursor-pointer" onClick={() => scrollToElement("experience")}>
               Experiência
             </a>
-            <a
-              className="text-white text-xl font-medium cursor-pointer"
-              onClick={() => {
-                toggleMenu();
-                const element = document.getElementById("projects");
-                if (!element) return;
-                window.scroll({
-                  top: element.offsetTop - 40,
-                  behavior: "smooth",
-                });
-              }}
-            >
+            <a className="text-white text-xl font-medium cursor-pointer" onClick={() => scrollToElement("projects")}>
               Projetos
             </a>
           </div>
